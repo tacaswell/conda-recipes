8844289e8141ec7c8c93e22e5fa5e2db3c98eeb0
Thomas A Caswell <tcaswell@bnl.gov>
2014-08-24 22:22:38 -0400

    BLD : more tweaking build

--- xraylib/build.sh
+++ xraylib/build.sh
@@ -1,4 +1,4 @@
 autoreconf -i
-./configure --enable-python --disable-lua --disable-ruby --disable-perl --disable-fortran2003 --prefix=$PREFIX
+./configure --enable-python --prefix=$PREFIX
 make -j 4
 make install
