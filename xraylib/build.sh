4b33781a1893ffeafa523036e94e12ee2e26ee92
Thomas A Caswell <tcaswell@bnl.gov>
2014-08-25 14:02:11 -0400

    BLD : don't build wrappers we don't care about

--- xraylib/build.sh
+++ xraylib/build.sh
@@ -1,4 +1,5 @@
 autoreconf -i
-./configure --enable-python --prefix=$PREFIX
+./configure --enable-python --disable-perl --disable-java \
+	    --disable-fortran2003 --disable-lua --prefix=$PREFIX
 make -j 4
 make install
